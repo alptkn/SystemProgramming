083b38985ec4699a7b039e673b5bcbb0485fb550
alptkn <alptknheat@gmail.com>
2021-05-20 14:44:40 +0300

    Add files via upload

---
+++ File_System_Proje/File_System_Proje/CMakeFiles/file_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/file_system.dir/file_system.c.o"
+  "file_system.pdb"
+  "file_system"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/file_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
